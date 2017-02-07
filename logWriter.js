e927d92a31ccdddc3db7826ebe3af3679fdb8e58
oevtuh <oevtuh@gmail.com>
2017-02-07 18:28:38 +0200

    added comments for logWriter

--- logWriter.js
+++ logWriter.js
@@ -2,11 +2,18 @@ const config = require('./config');
 const cloudWatchLogs = require('./cloudWatchLogs');
 
 class LogWriter {
+
+	/**
+	 * Constructor that starts logging scheduler.
+	 */
 	constructor() {
 		this.queue = {};
 		this.start();
 	}
 
+	/**
+	 * Interval handler that writes messages from queue and clears it.
+	 */
 	flush() {
 		const messages = Object.keys(this.queue)
 			.reduce((previous, current) => {
@@ -26,18 +33,28 @@ class LogWriter {
 		this.queue = {};
 	}
 
+	/**
+	 * Adds given method for given appId
+	 * @param {String} appId - Client application identifier
+	 * @param {String} method - Identifies the API method
+	 */
 	log(appId, method) {
 		const app = this.queue[appId] = this.queue[appId] || {};
 
 		app[method] = (app[method] || 0) + 1;
 	}
-
+	/**
+	 * Start logging scheduler
+	 */
 	start() {
 		this.intervalId = setInterval(() => {
 			this.flush();
 		}, config.schedule.updateInterval);
 	}
 
+	/**
+	 * Stops logging scheduler
+	 */
 	stop() {
 		clearInterval(this.intervalId);
 	}
