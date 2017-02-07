327943695ae17699e136f760c6078036e1633471
oevtuh <oevtuh@gmail.com>
2017-02-07 13:38:47 +0200

    added awsConfig file

---
+++ server.js
@@ -0,0 +1,29 @@
+// Load the SDK and UUID
+var AWS = require('aws-sdk');
+
+AWS.config.loadFromPath('./awsConfig.json');
+
+
+// Create CloudWatch service object
+var cw = new AWS.CloudWatch({apiVersion: '2010-08-01'});
+
+var params = {
+  Dimensions: [
+    {
+      Name: 'LogGroupName', 
+    },
+  ],
+  MetricName: 'IncomingLogEvents',
+  Namespace: 'AWS/Logs'
+};
+
+cw.listMetrics(params, function(err, data) {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    console.log("Metrics", JSON.stringify(data.Metrics));
+  }
+});
+
+
+
